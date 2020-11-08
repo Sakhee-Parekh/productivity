f6fdf287ea8f7e82d159f1515d7d9348132ddda3
Infinity <Infinity@haparekh-WX-2.corp.adobe.com>
2020-11-07 17:35:56 -0800

    chnages to goals files

--- code.js
+++ code.js
@@ -1,7 +1,14 @@
 
 function updateButtonText() {
+    // var goals  = [];
     let current = document.getElementById('goal').value; 
     let prev = document.getElementById('txtName').innerHTML; 
+    // goals.push(txtname);
     let final = prev + "<br>" + "<br>" + current; 
     document.getElementById('txtName').innerHTML = final;
 }
+
+// function clear(){
+//     goals.length = 0;
+// }
+
